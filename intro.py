d608f9031bb6e40417a05049eb9a8dd0b3ce72b2
arpitparekh <arpitparekh9@gmail.com>
2024-08-14 22:06:12 +0530

    first_commit

---
+++ intro.py
@@ -0,0 +1,58 @@
+# introduction
+
+# core python
+# variables
+# datatypes
+# operators
+# datatype methods
+# conditional
+# loops
+# loop inside loop
+# functions # def
+# lambda functions
+
+# oopc
+# class
+# objects
+# constructor
+# inheritance
+# encapsulation
+# abstarction
+# polymorphism # poly (many) morphism (form)
+
+# modules
+# exception handling
+
+# a = 12
+# try:
+#     result = a / 0
+# except:
+#     print("Zero thi devide na thay")
+
+# collection framework
+
+# Date
+# Math
+# Regex
+# Json
+
+# Advance Python
+
+# File Handling
+# MySql Database
+# MongoDb Database
+# Python GUI  (Desktop application) # Tkinter
+# Django (web application)  # backend # forntend html css bootstap
+# Api Calling (Networking in python)
+
+# Data Analytics
+# numpy
+# pandas
+# matplotlib
+# scrapy
+
+# AI ML
+# Scikit
+# tensorflow
+
+# Big data (job description) # 15000
